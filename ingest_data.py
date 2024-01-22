d7b97eca3c74ab4eeb7cf3138f06bf5dea30325e
Dave <d.wilson@nithio.com>
2024-01-22 14:36:19 -0500

    Exception updates

--- ingest_data.py
+++ ingest_data.py
@@ -41,18 +41,22 @@ def main(params):
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
     while True:
-        t_start = time()
-        
-        df = next(df_iter)
-        
-        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-        
-        df.to_sql(name=table_name, con=engine, if_exists='append')
-        
-        t_end = time()
-        
-        print('inserted another chunk..., took %.2f second' %(t_end - t_start))
+        try:
+            t_start = time()
+            
+            df = next(df_iter)
+            
+            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+            
+            df.to_sql(name=table_name, con=engine, if_exists='append')
+            
+            t_end = time()
+            
+            print('inserted another chunk..., took %.2f second' %(t_end - t_start))
+        except StopIteration:
+            print("Finished ingesting into postgres")
+            break
 
 if __name__ == '__main__':
 
